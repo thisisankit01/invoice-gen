81f59d7ca7c31ad45ef149a09ddac56bb86a603e
thisisankit01 <ankitechzz@gmail.com>
2023-10-26 00:02:20 +0530

    added , navbar and checking deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import InvoiceForm from './components/InvoiceForm';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import InvoiceForm from './components/InvoiceForm'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import InvoiceStore from './pages/InvoiceStore'
 
 class App extends Component {
-  render() {
-  return (
-    <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <Container>
-        <InvoiceForm/>
-      </Container>
-    </div>
-  );
-}}
+	render() {
+		return (
+			<div className="App d-flex flex-column align-items-center justify-content-center w-100">
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<InvoiceForm />}></Route>
+						<Route
+							path="/invoice"
+							element={<InvoiceStore />}
+						></Route>
+					</Routes>
+				</BrowserRouter>
+			</div>
+		)
+	}
+}
 
-export default App;
+export default App
